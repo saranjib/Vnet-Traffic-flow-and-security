ca57e442208683fac68f34173bb76964bc6510c0
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-01 23:23:26 +0530

    Add web subnet resource to network configuration
    Added a new subnet resource for the web application.

--- terraform/network.tf
+++ terraform/network.tf
@@ -10,6 +10,11 @@ resource "azurerm_virtual_network" "vnet1" {
 }
 
 
+# ---------------------------------------------------------
+# Subnets
+# ---------------------------------------------------------
+
+
 resource "azurerm_subnet" "web" {
   name                 = "web-subnet"
   resource_group_name  = azurerm_resource_group.rg-azure-vnet-project.name
