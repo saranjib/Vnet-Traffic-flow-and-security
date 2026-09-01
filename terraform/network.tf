fe1535f5c969770c47d327515f003fdd5c4b556a
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-01 23:21:20 +0530

    Rename and update subnets in network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,57 +1,33 @@
 # ---------------------------------------------------------
-# VNET 1
+# VNET 
 # ---------------------------------------------------------
 
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "vnet-project-01"
+  name                = "vnet-security-lab"
   location            = azurerm_resource_group.rg-azure-vnet-project.location
   resource_group_name = azurerm_resource_group.rg-azure-vnet-project.name
-
-  address_space = ["10.0.0.0/16"]
+  address_space       = ["10.10.0.0/16"]
 }
 
-# Public subnet
 
-resource "azurerm_subnet" "public" {
-  name                 = "public-subnet"
+resource "azurerm_subnet" "web" {
+  name                 = "web-subnet"
   resource_group_name  = azurerm_resource_group.rg-azure-vnet-project.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-
-  address_prefixes = ["10.0.1.0/24"]
+  address_prefixes     = ["10.10.1.0/24"]
 }
 
-# Private subnet
-
-resource "azurerm_subnet" "private" {
-  name                 = "private-subnet"
+resource "azurerm_subnet" "app" {
+  name                 = "app-subnet"
   resource_group_name  = azurerm_resource_group.rg-azure-vnet-project.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-
-  address_prefixes = ["10.0.2.0/24"]
-
-  private_endpoint_network_policies = "Disabled"
+  address_prefixes     = ["10.10.2.0/24"]
 }
 
-# ---------------------------------------------------------
-# VNET 2 - Used for VNet Peering
-# ---------------------------------------------------------
-
-resource "azurerm_virtual_network" "vnet2" {
-  name                = "vnet-project-02"
-  location            = azurerm_resource_group.rg-azure-vnet-project.location
-  resource_group_name = azurerm_resource_group.rg-azure-vnet-project.name
-
-  address_space = ["10.1.0.0/16"]
-
-}
-
-resource "azurerm_subnet" "vnet2_private" {
-  name                 = "private-subnet"
+resource "azurerm_subnet" "db" {
+  name                 = "db-subnet"
   resource_group_name  = azurerm_resource_group.rg-azure-vnet-project.name
-  virtual_network_name = azurerm_virtual_network.vnet2.name
-
-  address_prefixes = ["10.1.1.0/24"]
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.10.3.0/24"]
 }
 
-
-  
