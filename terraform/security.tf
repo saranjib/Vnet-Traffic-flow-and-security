9cd42a6d88bd5023061a17b9228db718af342b48
Saranjib Kuanar <102132535+saranjib@users.noreply.github.com>
2026-09-01 23:26:53 +0530

    Create network security group with inbound rules
    Added a network security group with rules for SSH, HTTP, and HTTPS access.

---
+++ terraform/security.tf
@@ -0,0 +1,46 @@
+# ---------------------------------------------------------
+# Network Security Groups
+# ---------------------------------------------------------
+
+
+resource "azurerm_network_security_group" "web" {
+  name                = "nsg-web"
+  location            = azurerm_resource_group.rg-azure-vnet-project.location
+  resource_group_name = azurerm_resource_group.rg-azure-vnet-project.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.admin_ip
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTPS"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
